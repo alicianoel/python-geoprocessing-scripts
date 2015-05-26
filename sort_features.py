99328535119d50722410f7dab6d0354a8e3bb222
alicianoel <ajnoel9@gmail.com>
2015-05-25 22:17:29 -0700

    Create sort_features.py

---
+++ sort_features.py
@@ -0,0 +1,51 @@
+import datetime
+import os
+
+from collections import OrderedDict
+import itertools
+from operator import itemgetter, attrgetter
+import glob
+import subprocess
+from collections import defaultdict
+from pprint import pprint
+import datetime
+
+import ogr
+import osr
+import gdal
+
+def sortlayer(l, fd):
+	"""Sorts feature stack order in a shapefile.
+
+	Args:
+		l: Shapefile to read attributes.
+		fd: field to sort by
+	"""
+
+	inDataSource = inDriver.Open(l, 1)
+	inlayer = inDataSource.GetLayer()
+	fids = []
+	vals = []
+
+	for f in inlayer:
+		fid = f.GetFID()
+		fids.append(fid)
+		vals.append((f.GetField(fd), fid))
+	vals.sort()
+	# index as dict: {newfid: oldfid, ...}
+	ix = {fids[i]: vals[i][1] for i in xrange(len(fids))}
+
+	# swap features around in groups/rings
+	for fidstart in ix.keys():
+		if fidstart not in ix: continue
+		ftmp = inlayer.GetFeature(fidstart)
+		fiddst = fidstart
+		while True:
+			fidsrc = ix.pop(fiddst)
+			if fidsrc == fidstart: break
+			f = inlayer.GetFeature(fidsrc)
+			f.SetFID(fiddst)
+			inlayer.SetFeature(f)
+			fiddst = fidsrc
+		ftmp.SetFID(fiddst)
+		inlayer.SetFeature(ftmp)
